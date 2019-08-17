7a554b81ae711193d389887d4a7bb0411a09bd9b
ChuckHend <hendel.adam@gmail.com>
2019-08-17 09:02:38 -0500

    compare loop, lc, parallel

--- parallel.py
+++ parallel.py
@@ -11,12 +11,36 @@ else:
 def processInput(i):
 	return i * i
 
+def for_loop(inputs):
+    results = []
+    for x in inputs:
+        results.append( processInput(x) )
+    return results
+
+def list_comp(inputs):
+    return [ processInput(x) for x in inputs ]
+
+def parallel(inputs): 
+    num_cores = multiprocessing.cpu_count()
+    # print('\nNumber Cores: {}'.format(num_cores))
+    results = Parallel(n_jobs=num_cores)(delayed(processInput)(i) for i in inputs)
+    return results
+
+
+methods = {
+    'loop' : for_loop,
+    'list_comp' : list_comp,
+    'parallel' : parallel
+    }
+
 if __name__ == '__main__':
     print('Computing x^2 for {} iterations. . .'.format(x))
-    num_cores = multiprocessing.cpu_count()
-    print('Number Cores: {}'.format(num_cores))
     inputs = range(x)
-    start = time.process_time()
-    results = Parallel(n_jobs=num_cores)(delayed(processInput)(i) for i in inputs)
-    time = round(time.process_time()-start,4)
-    print('{} seconds'.format(time))
+    
+    for alg, fun in methods.items():
+        print(f"Starting-{alg}--- ", end='\t')
+        start = time.process_time()
+        results = fun(inputs)
+        # results = Parallel(n_jobs=num_cores)(delayed(processInput)(i) for i in inputs)
+        proc_time = round(time.process_time()-start,4)
+        print(f'{proc_time} seconds')
